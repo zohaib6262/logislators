0d6952d00692f9f68b4b1bd10d21c81b65e29630
zohaib6262 <zohaibbinarhraaf@gmail.com>
2025-09-01 13:25:03 +0500

    add ; ;

--- frontend/src/utils/uploadImageCloudinary.js
+++ frontend/src/utils/uploadImageCloudinary.js
@@ -1,10 +1,10 @@
 export const uploadImageToCloudinary = async (file) => {
   const data = new FormData();
   data.append("file", file);
-  data.append("upload_preset", "my_upload_preset");
+  data.append("upload_preset", "my_upload_presets");
 
   const res = await fetch(
-    "https://api.cloudinary.com/v1_1/dlb6fup90/image/upload",
+    "https://api.cloudinary.com/v1_1/dy910ughf/image/upload",
     {
       method: "POST",
       body: data,
