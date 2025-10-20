3f32d58898e1fd32521573bb5a2571294d331414
zohaib6262 <zohaibbinarhraaf@gmail.com>
2025-10-21 02:11:58 +0500

    add

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,7 +8,7 @@ import BASE_URL from "@/lib/utils";
 // const API_BASE_URL = "http://localhost:3000/api";
 // import.meta.env.VITE_API_URL ||
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
