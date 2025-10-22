4a1f1b26acca872e235784336f0b0e2ea4351337
zohaib6262 <zohaibbinarhraaf@gmail.com>
2025-10-23 00:57:28 +0500

    add

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -9,18 +9,6 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
 const User = mongoose.model("User", userSchema);
 
 export default User;
