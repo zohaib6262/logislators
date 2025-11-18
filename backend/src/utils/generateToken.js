a820542763beb91ad515e8724eebfdeb159ebc63
zohaib6262 <zohaibbinarhraaf@gmail.com>
2025-11-18 11:14:41 +0500

    now the token not will be expire for 30days

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,7 +2,5 @@ import jwt from "jsonwebtoken";
 
 // Generate Token
 export const generateToken = (payload) => {
-  return jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
+  return jwt.sign(payload, process.env.JWT_SECRET);
 };
